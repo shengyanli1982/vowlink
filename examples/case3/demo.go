e24597d6e01f9463d9d0f5264bb92b4690f768bc
shengyanli1982 <shengyanlee36@gmail.com>
2024-03-06 10:45:03 +0800

    Optimize and improve code remarks information

--- examples/case3/demo.go
+++ examples/case3/demo.go
@@ -7,25 +7,43 @@ import (
 )
 
 func main() {
-	// vowlink like a chain, you can add more then() to the chain to do more things after the promise is resolved.
+	// vowlink 像一个链条，你可以在链条上添加更多的 then() 来在 promise 解析后做更多的事情。
+	// vowlink is like a chain, you can add more then() to the chain to do more things after the promise is resolved.
 	_ = vl.NewPromise(func(resolve func(interface{}), reject func(error)) {
+		// 这个 promise 直接解析为 "hello world"
+		// This promise is directly resolved to "hello world"
 		resolve("hello world")
 	}).Then(func(value interface{}) interface{} {
+		// 在 Then 方法中，我们将解析的值加上 " Copilot !!"
+		// In the Then method, we append " Copilot !!" to the resolved value
 		return value.(string) + " Copilot !!"
 	}, nil).Catch(func(error) error {
+		// 如果 promise 被拒绝，我们将返回一个新的错误信息 "rejected."
+		// If the promise is rejected, we return a new error message "rejected."
 		return fmt.Errorf("rejected.")
 	}).Finally(func() {
+		// 不论 promise 是被解析还是被拒绝，Finally 方法都会被调用，并打印 "finally 1"
+		// Whether the promise is resolved or rejected, the Finally method will be called and print "finally 1"
 		fmt.Println("finally 1")
 	})
 
-	// rejected promise
+	// 这是一个被拒绝的 promise
+	// This is a rejected promise
 	_ = vl.NewPromise(func(resolve func(interface{}), reject func(error)) {
+		// 这个 promise 被拒绝，原因是 "error"
+		// This promise is rejected with the reason "error"
 		reject(fmt.Errorf("error"))
 	}).Then(func(value interface{}) interface{} {
+		// 如果 promise 被解析，我们将解析的值加上 " Copilot !!"
+		// If the promise is resolved, we append " Copilot !!" to the resolved value
 		return value.(string) + " Copilot !!"
 	}, nil).Catch(func(error) error {
+		// 如果 promise 被拒绝，我们将返回一个新的错误信息 "rejected."
+		// If the promise is rejected, we return a new error message "rejected."
 		return fmt.Errorf("rejected.")
 	}).Finally(func() {
+		// 不论 promise 是被解析还是被拒绝，Finally 方法都会被调用，并打印 "finally 2"
+		// Whether the promise is resolved or rejected, the Finally method will be called and print "finally 2"
 		fmt.Println("finally 2")
 	})
 }
--- examples/case6/demo.go
+++ examples/case6/demo.go
@@ -7,28 +7,43 @@ import (
 )
 
 func main() {
-	// create 3 promise
+	// 创建 3 个 promise
+	// Create 3 promises
 	p1 := vl.NewPromise(func(resolve func(interface{}), reject func(error)) {
+		// 第一个 promise 直接解析为 "Promise"
+		// The first promise is directly resolved to "Promise"
 		resolve("Promise")
 	}).Then(func(value interface{}) interface{} {
+		// 在 Then 方法中，将解析的值加上 " 1"
+		// In the Then method, append " 1" to the resolved value
 		return value.(string) + " 1"
 	}, nil)
 
 	p2 := vl.NewPromise(func(resolve func(interface{}), reject func(error)) {
+		// 第二个 promise 直接解析为 "Promise"
+		// The second promise is directly resolved to "Promise"
 		resolve("Promise")
 	}).Then(func(value interface{}) interface{} {
+		// 在 Then 方法中，将解析的值加上 " 2"
+		// In the Then method, append " 2" to the resolved value
 		return value.(string) + " 2"
 	}, nil)
 
 	p3 := vl.NewPromise(func(resolve func(interface{}), reject func(error)) {
+		// 第三个 promise 直接解析为 "Promise"
+		// The third promise is directly resolved to "Promise"
 		resolve("Promise")
 	}).Then(func(value interface{}) interface{} {
+		// 在 Then 方法中，将解析的值加上 " 3"
+		// In the Then method, append " 3" to the resolved value
 		return value.(string) + " 3"
 	}, nil)
 
+	// Race() 将等待第一个 promise 被解析，并返回一个带有值的 promise
 	// Race() will wait for the first promise to be resolved, and return a promise with the value
 	result := vl.Race(p1, p2, p3)
 
-	// get the value from the promise
+	// 从 promise 中获取值并打印
+	// Get the value from the promise and print it
 	fmt.Println(">>", result.GetValue().(string))
 }
